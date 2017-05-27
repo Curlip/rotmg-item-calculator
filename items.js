98f39b57991e839a8100eface0635bcc2b84e69e
Curlip <curlipcool@gmail.com>
2017-05-27 21:37:43 +1000

    - Added sprite Rendering - Changed layout to reflect the new feature

--- items.js
+++ items.js
@@ -1,48 +1,65 @@
+baseURL = "https://static.drips.pw/rotmg/production/current";
 var items = []
 
 //basic item from 'equip.xml'
-function Item(name, sprite, desc, tier){
+function Item(name, desc, tier){
     this.name = name;
-    this.sprite = sprite;
     this.desc = desc;
     this.tier = tier;
 
+    //Return html for an Item Sprite
+    Item.prototype.drawSprite = function(){
+        row = 0 - (this.spriteRef >>> 4);
+        column = 0 - (this.spriteRef & 0x00F);
+
+        div = ""
+
+        div +=      "<div class='item-sprite'"
+        div +=          "style='"
+        div +=              "background-image: url(" + baseURL + "/sheets/" + this.spriteFile + ");"
+        div +=              "background-position:" + column*48 + "px " + row*48 + "px;"
+        div +=      "'></div>"
+
+
+        return div;
+    }
+
     //Append Beutiful HTML Representation of 'item' to 'container'
-    Item.prototype.appendItem = function(container){
+    Item.prototype.drawItem = function(container){
         div = "";
 
         div += "<div class='item'>"
-        div +=     "<h3>"
-        div +=         "<span class='itemName'>" + this.name + "</span>"
-        div +=         "<span class='tier'>" + (this.tier == "" ? "<span style='color: #8B2DDC;' >UT</span>" : (this.consumable ? "" : "T" + this.tier)) + "</span>"
+        div +=     "<h3 class='item-header'>"
+        div +=         this.drawSprite()
+        div +=         "<div class='header-text'>"
+        div +=             "<div class='item-name'>" + this.name + "</div>"
+        div +=             "<div class='tier'>" + (this.tier == "" ? "<span style='color: #8B2DDC;' >UT</span>" : (this.consumable ? "" : "T" + this.tier)) + "</div>"
+        div +=         "</div>"
         div +=     "</h3>"
-        div +=     this.desc + "<br />"
-//        div +=     "<hr/>"
-//        div +=     (this.famebonus != "" ? this.famebonus + "% Famebonus <br />" : "")
-//        div +=     "<hr/>"
-//        div +=     (this.soulbound != "" ? "Soulbound <br />" : "")
-//        div +=     (this.feedpower != "" ? "<span class='fp'>" + this.feedpower + "FP </span><br />" : "")
-//        div += "</div>"
-
+        div += "</div>"
 
         container.append(div)
     }
 }
 
-//Clears and repopulates items array using 'xml'
+//reloads items array using 'xml'
 //This should allow for selectable xml files
-function repopulateItems(xml){
+function reloadItems(xml){
     items = [];
 
     $(xml).find("Object").each(function(i,e){
         $obj = $(e);
 
         name = $obj.attr("id");
-        sprite = $obj.find("File").text() + "_" + $obj.find("Index").text();
         desc = $obj.find("Description").text();
         tier = $obj.find("Tier").text();
 
-        item = new Item(name, sprite, desc, tier);
+        item = new Item(name, desc, tier);
+
+        item.spriteFile = $obj.find("File").text() + ".png";
+        item.spriteRef = $obj.find("Index").text();
+
+        //item.spriteFile = item.spriteFile.replace("playerskins", "playersSkins")
 
         item.consumable = $obj.find("Consumable").length;
         item.soulbound = $obj.find("Soulbound").length;
@@ -55,15 +72,15 @@ function repopulateItems(xml){
 
 //Populate the items array with STATIC.DRIPS.PW data
 $.ajax({
-    url: "https://static.drips.pw/rotmg/production/current/xml/Equip.xml",  //TODO Make configurable
+    url: baseURL + "/xml/Equip.xml",  //TODO Make configurable
     type: "get",
     datatype:"xml",
     success: function(xml){
-        repopulateItems(xml);
+        reloadItems(xml);
 
         //append all items to page
         for(var i = 0; i < items.length; i++){
-            items[i].appendItem($("body"))
+            items[i].drawItem($("body"))
         }
     }
 })
