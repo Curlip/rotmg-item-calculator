f10f8c426ab1128d03be0a694fec5637ec5e6520
Curlip <curlipcool@gmail.com>
2017-06-13 21:15:43 +1000

    Rewrote Item HTML in JQuery

--- items.js
+++ items.js
@@ -40,43 +40,53 @@ function Item(xml, url){
     }
 }
 
-    //Return html for an Item Sprite
-    Item.prototype.drawSprite = function(){
-        row = 0 - (this.spriteRef >>> 4);
-        column = 0 - (this.spriteRef & 0x00F);
+//Return html for an Item Sprite
+Item.prototype.drawSprite = function(){
+    row = 0 - (this.spriteRef >>> 4);
+    column = 0 - (this.spriteRef & 0x00F);
 
-        div = ""
+    container = $("<div class='item-sprite'>")
+        container.css("background-image", "url(" + this.url + "/sheets/" + this.spriteFile + ")")
+        container.css("background-position", column*48 + "px " + row*48 + "px ")
 
-        div += "<div class='item-sprite'"
-        div +=     "style='"
-        div +=         "background-image: url(" + this.url + "/sheets/" + this.spriteFile + ");"
-        div +=         "background-position:" + column*48 + "px " + row*48 + "px;"
-        div += "'></div>"
+    return container;
+}
 
+//Return html for an Item Header (sprite + name + tier)
+Item.prototype.drawHeader = function(){
+    var container = $("<h3 class='item-header'>");
+        container.append(this.drawSprite())                                                 //[Sprite]
 
-        return div;
+    var text = $("<div class='header-text'>")
+        text.append("<div class='item-name'>" + this.name + "</div>")                       //NAME
+
+        var tier = $("<div class='tier'>")                                                  //Tier
+            if(!this.consumable){
+                if(this.tier) { tier.append("T" + this.tier) }
+                if(!this.tier){ tier.append("<span style='color: #8B2DDC;' >UT</span>")}
+            }
+        text.append(tier);
+    container.append(text);
+
+    return container;
+}
+
+//Append Beutiful HTML Representation of 'item' to 'container'
+Item.prototype.drawItem = function(container){
+    var top = $("<div class='item'>");                                                      //[SPRITE]     Title    T*
+        top.attr("id", "item" + this.identifier)
+        top.attr("title", (this.id ? this.id : this.name))
+    top.append(this.drawHeader())
+
+    var desc = $("<div class='desc'>")                                                      //Description about the item
+        desc.append("<p>" + (this.desc).split("\\n").join("<br />") + "</p>")
+    top.append(desc)
+
+    top.append("<hr />")                                                                    //--------------------------
+
+    if(this.feedpower){
+        top.append($("<span class='fp'>").append("Feed Power: " + this.feedpower))          //Feed Power: ***
     }
 
-    //Append Beutiful HTML Representation of 'item' to 'container'
-    Item.prototype.drawItem = function(container){
-        div = "";
-
-        div += "<div class='item' title='"+(this.id ? this.id : this.name)+"'>"
-        div +=     "<h3 class='item-header'>"
-        div +=         this.drawSprite()
-        div +=         "<div class='header-text'>"
-        div +=             "<div class='item-name'>" + this.name + "</div>"
-        div +=             "<div class='tier'>" + (this.tier == "" ? "<span style='color: #8B2DDC;' >UT</span>" : (this.consumable ? "" : "T" + this.tier)) + "</div>"
-        div +=         "</div>"
-        div +=     "</h3>"
-        div +=     "<div class='desc'>"
-        div +=         "<p>" + (this.desc).split("\\n").join("<br />") + "</p>"
-        div +=     "</div>"
-        div +=     "<hr />"
-        if(this.feedpower){
-            div +=      "<span class='fp'> Feed Power: " + this.feedpower + "</span>"
-        }
-        div += "</div>"
-
-        container.append(div)
-    }
+    container.append(top)
+}
